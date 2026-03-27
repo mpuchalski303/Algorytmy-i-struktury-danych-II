b4c6f526f910719f2c4971aa2e29c4e6ba12efd2
Milosz Puchalski <mpuchalski303@gmail.com>
2026-03-27 19:12:04 +0100

    Dodanie nowych rzeczy do Bst1

--- Bst1.cpp
+++ Bst1.cpp
@@ -1,4 +1,16 @@
+/*
+
+1. Zaimplementuj BST z operacjami insert, find, delete, znajdującymi wartość maksymalną i
+minimalną
+
+a. Oblicz sumę k-najmniejszych elementów w drzewie.
+b. Połącz dwa drzewa tak, żeby wynikowe drzewo było dalej poprawnym drzewem BST. 
+
+*/
+
 #include<iostream>
+#include<vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -21,8 +33,6 @@ galaz* InsertBST(galaz* root,int a){
     }
     if(a > root->wartosc){
         root->prawy = InsertBST(root->prawy, a);
-
-
     }
     else{
         root->lewy =  InsertBST(root->lewy, a);
@@ -37,6 +47,104 @@ void wyswielt(galaz* root){
 
     }
 }
+void maks(galaz* root){
+    if(root->prawy == nullptr){
+        cout<<"MAX: "<<root->wartosc<<endl;
+    }
+    else{
+        maks(root->prawy);
+    }
+}
+void min(galaz* root){
+    if(root->lewy == nullptr){
+        cout<<"MIN: "<<root->wartosc<<endl;
+    }
+    else{
+        min(root->lewy);
+    }
+}
+
+void FindBST(galaz* root,int szukana){//zakladamy ze wiemy ze sie znajduje ta wartosc
+    int iter = 1;
+
+    while(root->wartosc != szukana){
+        if(root->wartosc < szukana){
+            root = root->prawy;
+            iter++;
+        }
+        else{
+            root = root->lewy;
+            iter++;
+        }
+    }
+    cout<<"Glebokosc:"<<iter<<endl;
+    
+
+}
+galaz* FindBST2(galaz* root,int szukana){
+    int iter = 1;
+
+    while(root->wartosc != szukana){
+        if(root->wartosc < szukana){
+            root = root->prawy;
+            iter++;
+        }
+        else{
+            root = root->lewy;
+            iter++;
+        }
+    }
+    return root;
+    
+
+}
+galaz* delateBST(galaz* root, int do_usuniecia){//zakladamy ze usuwamy liscie drzewa
+
+    
+        if(root->prawy->wartosc == do_usuniecia){
+            root->prawy = nullptr;
+            return root;
+        }
+        if(root->lewy->wartosc == do_usuniecia){
+            root->lewy = nullptr;
+            return root;
+        }
+        if(root->wartosc < do_usuniecia){
+
+            delateBST(root->prawy, do_usuniecia);
+            
+        }
+        else{
+            delateBST(root->lewy, do_usuniecia);
+            
+        }
+    
+    return root;
+    
+    
+}
+void stworz_wektor(galaz* root, vector<int> &wektor){
+    if(root != nullptr){
+        stworz_wektor(root->lewy, wektor);
+        wektor.push_back(root->wartosc);
+        stworz_wektor(root->prawy, wektor);
+
+    }
+}
+
+int SumaNajmniejszychElementow(galaz* root, int k){
+    vector<int> wektor;
+    int suma=0;
+    stworz_wektor(root,wektor);
+
+    
+    sort(wektor.begin(), wektor.end());
+    for(int i =0; i<k;i++){
+        
+        suma += wektor[i];
+    }
+    return suma;
+}
 
 int main(){
 
@@ -45,10 +153,23 @@ int main(){
     
     InsertBST(root, 2);
     InsertBST(root, 8);
-    InsertBST(root, 1);
+    InsertBST(root, 12);
     InsertBST(root, 4);
     InsertBST(root, 6);
     InsertBST(root, 9);
+
     wyswielt(root);
+    cout<<endl;
+
+    maks(root);
+    min(root);
+
+    //FindBST(root,2);
+    //wyswielt(FindBST2(root, 8));
+
+    //wyswielt(delateBST(root, 6));
+
+    cout<<"Suma: "<< SumaNajmniejszychElementow(root, 3);
+
     return 0;
 }
