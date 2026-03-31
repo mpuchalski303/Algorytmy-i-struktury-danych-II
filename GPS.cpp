1a9adba823c95fa7deb2621dd1aa87d70145b772
Milosz Puchalski <mpuchalski303@gmail.com>
2026-04-01 00:36:11 +0200

    koniec algorytmu najkrotszych sciezek

--- GPS.cpp
+++ GPS.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 #include<vector>
-
+/*
+Po rozmowie z księciem okazało się, że jego ukochana bardzo dużo czasu spędza w kuchni,
+nadzorując gotowanie owsianki dla wygłodniałych brodaczy. Ilość potrzebnej owsianki zwiększa się, gdy krasnoludki muszą pokonać większą odległość z domu do pracy (można przyjąć,
+że jest to jedyny czynnik). Książę, chcąc ulżyć swojej wybrance, zmierzył i spisał odległości,
+pomiędzy każdym z wyrobisk i domkiem każdego z krasnoludków. Prosi, abyś uwzględnił te
+informacje. Przy podziale pracy chciałby, aby sumaryczna odległość, którą każdego dnia muszą pokonać krasnoludki była możliwie jak najmniejsza, przy czym nie możemy pozwolić, żeby
+spadła wartość produkowanych dóbr.
+*/
 
 using namespace std;
 
@@ -30,24 +37,57 @@ int main(){
     vector<bool> odwiedzone(6, false);
     vector<int> sciezki;
     vector<vector<int>> odp;
+    vector<vector<int>> odleglosci(6, vector<int>(6,0));
 
+    int wynik;
     int glebia =0;
-    tablica_sasiedztwa[0] = {1, 2};
-    tablica_sasiedztwa[1] = {2, 3};
+    int suma;
+    int poczatek = 0;
+    int koniec = 5;
+    int min = 9999;
+    
+
+    tablica_sasiedztwa[0] = {1};
+    tablica_sasiedztwa[1] = {2,3};
     tablica_sasiedztwa[2] = {4};
     tablica_sasiedztwa[3] = {4};
     tablica_sasiedztwa[4] = {5};
     tablica_sasiedztwa[5] = {};
     
-    int poczatek = 0;
-    int koniec = 5;
-    dfs(poczatek,glebia, tablica_sasiedztwa, odwiedzone, sciezki, odp, koniec);
+    odleglosci[0][1] = 200;
+    odleglosci[1][2] = 60;
+    odleglosci[2][4] = 200;
+    odleglosci[1][3] = 100;
+    odleglosci[3][4] = 100;
+    odleglosci[4][5] = 500;
 
+    
+    dfs(poczatek,glebia, tablica_sasiedztwa, odwiedzone, sciezki, odp, koniec);
+    
     for(int i = 0;i<odp.size();i++){
+        suma = 0;
         for(int j= 0;j<odp[i].size();j++){
-            cout<<odp[i][j]<<endl;
+            if(j + 1 != odp[i].size()){
+                suma += odleglosci[odp[i][j]][odp[i][j+1]];
+                
+            }
+            
         }
-        cout<<endl;
+        if(suma < min){
+            min = suma;
+            wynik = i;
+        }
+        
     }
+    
+    cout<<"Najkrotsza droga to: ";
+    
+    for(int i= 0;i<odp[wynik].size();i++){
+        cout<<odp[wynik][i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Odleglosc to: "<<min;
+    
+    
     return 0;
 }
